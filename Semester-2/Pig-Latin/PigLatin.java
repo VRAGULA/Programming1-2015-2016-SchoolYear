d938472d7ce6bb4ae590974be136f5122a3a8ab6
Vikrant Ragula <ragula.v1@gmail.com>
2016-05-20 12:53:14 -0600

    Add files via upload

---
+++ Semester-2/Pig-Latin/PigLatin.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+class PigLatin {
+  public static void main(String[] args){
+        System.out.println("Enter a word to translate to Pig Latin");
+    Scanner scanner = new Scanner(System.in);
+    String str1 = scanner.nextLine();
+    str1 = str1.toLowerCase;
+    String str2 = str1.substring(1);
+    j
+    //System.out.println("Your new word is " + str1.substring(0,1) + str2 + "ay");
+    char = word;
+    boolean ifVowel = word == 'a' || word == 'e' || word == 'i' || word == 'o' || word == 'u';
+    
+    if(ifVowel) {
+      
+    }
+  }
+}
---
+++ Semester-2/Pig-Latin/Test.java
@@ -0,0 +1,65 @@
+// Starter Activity
+//public class Test{
+//  
+//  public static void main(String args[]) {
+//    String fullName = new String("Vikrant Ragula");
+//    System.out.println(fullName);
+//  }
+//}
+// Make all letters uppercase
+//public class Test{
+//  public static void main(String args[]) {
+//    String fullName = new String("Vikrant Ragula");
+//    fullName = fullName.toUpperCase();
+//    System.out.println(fullName);
+//}
+//}
+//Make all letters lowercase
+//public class Test{
+//  public static void main(String args[]) {
+//    String fullName = new String("Vikrant Ragula");
+//    fullName = fullName.toLowerCase();
+//    System.out.println(fullName);
+//}
+//}
+//Code to make it say person age as well as name
+//public class Test{
+//  public static void main(String args[]) {
+//    String fullName = new String("\\Vikrant Ragula ");
+//    int personAge = 14;
+//    System.out.println(fullName + "is " + personAge + ".");
+//}
+//}
+// Code to Count how many letters in name
+//public class Test{
+//
+//public static void main(String args[]) {
+//    String fullName = new String("VikrantRagula");
+//    int letters = fullName.length();
+//    System.out.println(letters);
+//}
+//}
+
+// Code to Append a String
+
+//public class Test{
+//  public static void main(String args[]) {
+//    String firstWord = new String("Cape");
+//    String lastWord = new String("Cod");
+//    String fullWord = new String(firstWord + " " + lastWord);
+//    System.out.println(fullWord);
+//  }
+//}
+
+//Code to separate a string
+//public class Test{
+//  public static void main(String args[]) {
+//    String string = "Vikrant-Ragula";
+//    String[] parts = string.split("-");
+//    String part1 = parts[0];
+//    String part2 = parts[1];
+//    System.out.println(part1 + " " + part2);
+//  }
+//}
+
+
